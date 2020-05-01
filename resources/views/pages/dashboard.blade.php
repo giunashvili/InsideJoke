f8138273afd52641ff0546f284341da86127c85c
Tamarella <tamar.archvadze.2@iliauni.edu.ge>
2020-05-01 23:19:09 +0400

    worked on navbar universalization

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,38 +5,7 @@
         <link rel="stylesheet" href={{ asset('assets/css/fonts.css') }} />
     </header>
     <body>
-        <nav>
-            <a href="{{ route('dashboard') }}">
-                <div class="nav-item"> 
-                    <img src="{{ asset('assets/icons/home.png') }}" />
-                    <span>მთავარი</span>
-                </div>
-            </a>
-            <a href="{{ route('members') }}">
-                <div class="nav-item"> 
-                    <img src="{{ asset('assets/icons/members.png') }}" />
-                    <span>ჯგუფის წევრები</span>
-                </div>
-            </a>
-            <a href="{{ route('social-links') }}">
-                <div class="nav-item"> 
-                    <img src="{{ asset('assets/icons/youtube.png') }}" />
-                    <span>სოციალური ბმულები</span>
-                </div>
-            </a>
-            <a href="{{ route('dashboard') }}">
-                <div class="nav-item"> 
-                    <img src="{{ asset('assets/icons/violinkey.png') }}" />
-                    <span>ბენდის შესახებ</span>
-                </div>
-            </a>
-            <a href="{{ route('login') }}">
-                <div class="nav-item"> 
-                    <img src="{{ asset('assets/icons/logout.png') }}" />
-                    <span>გადი გარეთ</span>
-                </div>
-            </a>       
-        </nav>
+        @include('layouts.navbar')   
         <div class="main-window">
             <span>დილამშვიდობისა!</span>
             <img src="{{ asset('assets/imgs/TV.png') }}" />
