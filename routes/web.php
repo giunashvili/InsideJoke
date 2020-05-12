975c227bb18002f0ea853dfee4655544273418f5
Tamarella <tamar.archvadze.2@iliauni.edu.ge>
2020-05-12 23:31:15 +0400

    worked on admin/social_links (part 2)

--- routes/web.php
+++ routes/web.php
@@ -16,34 +16,33 @@ use App\SocialLink;
 |
 */
 
-
 Route::get('register', function () {
-
     return view('pages.register.register');
 }) -> name ('register');
 
 Route::get('login', function () {
-
     return view('pages.login.login');
 }) -> name ('login');
 
-
-Route::get('/members', function () {
-    
-    $group_members = Member::all();
-
-    return view('pages.members.index') -> with("members", $group_members);
-})  -> name ('members');
-
-# Social Links Route
-Route::get('/social-links', function () {
-
+Route::get('social_links', function () {
     $social_links = SocialLink::all();
-
     return view('pages.social_links.index') -> with('social_links', $social_links);
+}) -> name ('soclinks');
 
-}) -> name ('social-links');
+Route::group(['prefix' => 'admin'], function(){
+    
+    Route::get('members', function () {
+        $group_members = Member::all();
+        return view('pages.members.index') -> with("members", $group_members);
+    })  -> name ('members');
 
-Route::get('/dashboard', function () {
-    return view('pages.dashboard');
-})  -> name ('dashboard');
+    Route::get('dashboard', function () {
+        return view('pages.dashboard');
+    })  -> name ('dashboard');
+
+    Route::get('social_links', function () {
+        $social_links = SocialLink::all();
+        return view('pages.social_links.soclinks_dashboard') -> with('social_links', $social_links);
+    })  -> name ('admin/soclinks');
+
+});
