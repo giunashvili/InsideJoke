791639ce53eb1c88413f5db14c86154827195be1
Tamarella <tamar.archvadze.2@iliauni.edu.ge>
2020-06-12 19:37:51 +0400

    fixed route lines

--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,13 @@ use App\SocialLink;
 |
 */
 
-Route::get('register', 'AuthController@register') 
-    -> name ('register');
-Route::get('login', 'AuthController@login') 
-    -> name ('login');
-    
+Route::get('register', 'AuthController@register') -> name ('register');
+Route::get('login', 'AuthController@login') -> name ('login');
+
 Route::group(['prefix' => 'admin'], function(){    
-    Route::get('dashboard', 'DashboardController@index') 
-        -> name ('dashboard');    
-    Route::get('members', 'MembersController@index')  
-        -> name ('members');
-    Route::get('social_links', 'SocialLinksController@index') 
-        -> name ('soclinks');
+    Route::get('dashboard', 'DashboardController@index') -> name ('dashboard');    
+    Route::get('members', 'MembersController@index') -> name ('members');
+    Route::get('social_links', 'SocialLinksController@index') -> name ('soclinks');
 });
 
 ?>
