6d0f45920f67e3f1eddfb96ef45281cc9a47ee3e
Tamarella <tamar.archvadze.2@iliauni.edu.ge>
2020-06-12 19:45:29 +0400

    deleted unuseful route lines

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Member;
-use App\SocialLink;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/* -------------------------- Web Routes ---------------------------------- */
 
 Route::get('register', 'AuthController@register') -> name ('register');
 Route::get('login', 'AuthController@login') -> name ('login');
