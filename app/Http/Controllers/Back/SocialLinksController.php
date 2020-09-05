7c0bf39c779cb61b08035690b18b7998a111b635
giunashvili <giorgi.official@yahoo.com>
2020-09-05 11:10:45 +0000

    chore: refactored a bit

--- app/Http/Controllers/Back/SocialLinksController.php
+++ app/Http/Controllers/Back/SocialLinksController.php
@@ -1,7 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Back;
 
+use App\Http\Requests\SocialLinks\UploadImage;
+use App\Http\Controllers\Controller;
 use App\Models\SocialLink;
 
 class SocialLinksController extends Controller
@@ -61,26 +63,15 @@ class SocialLinksController extends Controller
      * 
      * @return JSON
      */
-    public function uploadImage($id)
-    {
-        $img = request() -> img;
-
-        if( ! $img -> isValid() )
-        {
-            return response() -> json(
-                [
-                    'code'   => 422,
-                    'status' => 'image is not valid!',
-                ]
-            );
-        }
-
-        $socialLink = SocialLink :: findOrFail($id);
-        $extension  = $img -> extension();
-        $name = 'Social Link - '. $id . ' - ' . now() -> timestamp . '.' . $extension;
+    public function uploadImage(UploadImage $request)
+    {   
+        $id         = $request -> id;
+        $img        = $request -> img;
+        $socialLink = SocialLink :: find($id);
+        $name       = $this -> generateImageName();
         $img -> storeAs('social_links', $name );
 
-        $path = storage_path('social_links/' . $name);
+        $path       = storage_path('social_links/' . $name);
         $socialLink -> image() -> create([ 'path' => $path ]);
 
         return response() -> json(
@@ -89,5 +80,15 @@ class SocialLinksController extends Controller
                 'status' => 'Image successfully submitted!',
             ]
         );
+    }
+
+    /**
+     * Generate image name.
+     * 
+     * @return string
+     */
+    private function generateImageName()
+    {
+        return 'Social Link - '. request() -> id . ' - ' . now() -> timestamp . '.' . request() -> img -> extension();
     }
 }
