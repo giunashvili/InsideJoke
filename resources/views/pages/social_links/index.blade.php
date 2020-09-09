e8b72360c941e03ff8234e6f1449c5317cc41e20
Tamarella <tamar.archvadze.2@iliauni.edu.ge>
2020-09-09 19:57:03 +0400

    markup: add new social link

--- resources/views/pages/social_links/index.blade.php
+++ resources/views/pages/social_links/index.blade.php
@@ -3,6 +3,7 @@
         <title>Dashboard</title>
         <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
         <link rel="stylesheet" href="{{ asset('assets/css/soclinks.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/css/add.soclink.css') }}" />
         <link rel="stylesheet" href="{{ asset('assets/css/fonts.css') }}" />
         <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,100italic,300italic,400italic,500italic,500,700,700italic,900,900italic' rel='stylesheet' type='text/css' />
     </head>
@@ -13,18 +14,28 @@
                 <span>სოციალური ბმულები</span>
             </h1>
             <div class="scroll-space">
-               <img 
+               {{-- <img 
                     src="https://i.pinimg.com/originals/51/77/40/5177402f9a223466db995ed7c25a6311.gif"
                     style="width:440px; display:block; margin:auto"
-                />       
+                />        --}}
+            <form>
+
+                <input type="text" id=addNewSoclinkName name="addNewSoclinkName" placeholder="ბმულის სათაური"><br>
+                <input type="url" id=addNewSoclinkURL name="addNewSoclinkURL" placeholder="ბმული"><br>
+                <input type="submit" value="დაამატე სოციალური ბმული">
+
+            </form>
+
             </div>
-            <div class="add-button">დაამატე ახალი სოციალური ბმული</div>
+            {{-- <div class="add-button">დაამატე ახალი სოციალური ბმული</div> --}}
         </div>
     </body>
 </html>
 
 <script>
     window.onload = function() {
+
+        return;
 
         const socialLinksContainer = document.getElementsByClassName('scroll-space')[0];
         let socialLinks = '';
