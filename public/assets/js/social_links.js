de701daabe4778f43213e20d5fae9f58f745febb
Tamarella <tamar.archvadze.2@iliauni.edu.ge>
2020-09-17 22:21:43 +0400

    add new social link

---
+++ public/assets/js/social_links.js
@@ -0,0 +1,95 @@
+window.onload = function () {
+    renderSocialLinks(); 
+};
+
+function renderSocialLinks(){
+
+    const socialLinksContainer = document.getElementsByClassName('scroll-space')[0];
+    let socialLinks = '';
+
+    fetch('/api/social_link/all')
+        .then( result => result.json() )
+        .then( data => {
+
+           console.log(data);
+
+            for(i=0; i<data.length; i++)
+            {
+
+            socialLinks +=  `
+            <div class="main-container">
+                <div class="icon-box">
+                    <img class="icon" src="${data[i].img}" />
+                    <img class="icon-edit" src="{{ asset('assets/icons/Edit Photo btn.svg') }}" />
+                </div>
+                <div class="name"> ${data[i].name} </div>
+                <a href="${data[i].link}" target="_blank"> ${data[i].linkshortened} </a>
+                <div class="edit">
+                    <div class="edit-outer"></div>
+                    <div class="edit-inner"></div>
+                </div>
+                <div class="delete">
+                    <div class="delete-outer"></div>
+                    <div class="delete-inner"></div>
+                </div>
+            </div>
+            `;
+        }
+        
+        socialLinksContainer.innerHTML = socialLinks;
+});
+}
+
+function newSocialLink() {
+
+    /*
+     * 1) REPLACE Social Links list with form
+     */
+    const socialLinksContainer = document.getElementsByClassName('scroll-space')[0];
+    const form = `
+    <form>
+    <input type="text" id="addNewSoclinkName" name="addNewSoclinkName" placeholder="ბმულის სათაური"><br>
+    <input type="url" id="addNewSoclinkURL" name="addNewSoclinkURL" placeholder="ბმული"><br>
+    <input type="button" onclick="createNewSocialLink()" value="დაამატე სოციალური ბმული">
+    </form> 
+    `;
+    
+    socialLinksContainer.innerHTML = form; 
+
+    /*
+     * 2) CHANGE page header
+     */
+
+     const header = document.getElementsByClassName('social-links-header')[0];
+     header.innerHTML = 'დაამატე ახალი სოციალური ბმული';
+
+     /*
+     * 3) HIDE 'დაამატე ახალი სოციალური ბმული' button
+     */
+     const createSocialLinkbtn = document.getElementsByClassName('add-button')[0];
+     createSocialLinkbtn.classList.add('hidden');
+
+}
+
+function createNewSocialLink()
+{
+    const name = document.getElementById("addNewSoclinkName").value;
+    const link = document.getElementById("addNewSoclinkURL").value;
+
+    fetch('api/social_link/create', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            link: link
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        renderSocialLinks();
+    })
+    .catch(err => console.log(err));
+}
